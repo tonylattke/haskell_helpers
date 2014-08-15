e8e41f8b35a18347920cdf8fb0f9da4e03e94c75
Tony Lattke <tonylattke@gmail.com>
2014-08-15 22:32:06 +0200

    Init

---
+++ 1_values_types.hs
@@ -0,0 +1,82 @@
+-- ################################## Types and values ##################################
+
+-- Using Strings and Concatenation
+hello :: String -> String
+hello foo = "Hello " ++ foo
+
+-- Using Integer
+hoursToMinutes :: Int -> Int
+hoursToMinutes hours = 60 * hours
+
+-- Using Float
+celciusToFarenheit :: Float -> Float
+celciusToFarenheit celcius = 32 + celcius * (9.0/5)
+
+-- Multiline string
+multi = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+\Phasellus mattis ac tortor sed fringilla. Etiam aliquam\
+\turpis sit amet nisi molestie, nec accumsan massa tempus.\n"
+
+-- ################################# Operating Variables ################################
+
+-- Plus
+plus :: Num a => a-> a-> a
+plus a b = a + b
+
+-- Minus
+minus :: Num a => a-> a-> a
+minus a b = a - b
+
+-- Multiply
+multiply :: Num a => a-> a-> a
+multiply a b = a * b
+
+-- Divide
+divide :: Fractional a => a-> a-> a
+divide a b = a / b
+
+-- Module
+op_mod :: Integral a => a-> a-> a
+op_mod a b = mod a b
+--op_mod a b = a `mod` b
+
+-- Pow
+pow :: Integral a => a -> a -> a
+pow a b = a ^ b
+-- pow a b = a ** b
+
+-- pow (sqrt 42) 2
+
+-- Example replace
+-- replace "," "." "127,0,0,1" -> "127.0.0.1"
+
+subString :: Int -> Int -> String -> String
+subString _ _ "" = ""
+subString _ _ [a] = [a]
+subString 0 b c = aux_subString 0 b c
+subString a b (_:xs)
+	| a <= b = subString (a-1) (b-1) xs
+	| otherwise = ""
+
+aux_subString :: Int -> Int -> String -> String
+aux_subString _ _ [] = ""
+aux_subString a b (x:xs)
+	| a < b = [x] ++ (aux_subString (a+1) b xs)
+	| a == b = [x]
+	| otherwise = ""
+
+-- subString 0 1 "Lorem ipsum dolor sit amet" -> "Lo"
+-- subString 6 11 "Lorem ipsum dolor sit amet" -> "ipsum "
+-- subString 0 5 "Lorem ipsum dolor sit amet" -> "Lorem "
+-- subString 6 -1 "Lorem ipsum dolor sit amet" -> ""
+
+tillLast :: Int -> String -> String
+tillLast _ [] = ""
+tillLast 0 a = a
+tillLast a (_:xs) = tillLast (a-1) xs
+
+takeFrom :: Int -> Int -> String -> String
+takeFrom _ 0 _ 	= ""
+takeFrom _ _ "" = ""
+takeFrom 0 a b 	= take a b
+takeFrom a b (_:xs) = takeFrom (a-1) b xs
---
+++ README.md
@@ -0,0 +1,25 @@
+# Haskell Helpers
+
+Interactive Console:
+ghci
+
+On the interactive console load file use:
+:load file_name
+
+
+Compile and run file:
+runhaskell file_name
+
+Run file and interact:
+ghci file_name
+
+Compile file:
+ghc file_name
+
+Get tipe of the function:
+:t function_name
+
+| File name        		| Description 												   |
+| --------------------- |------------------------------------------------------------- |
+| 0_hello_world.hs 		| - Hello world to Haskell |
+| 1_values_types.hs 	| - Values and types on Haskell |
